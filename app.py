3c245eaee18dfd78a4ca348200b6f962316c674f
Arunkumar.R <arunkumarr@dckap.com>
2024-08-28 15:37:05 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# Simulated user database
+users_db = {"username": "kumararun31380@gmail.com", "password": "Arun@123"}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = users_db.get(username)
+        if username == users_db.get("username") and password == users_db.get("password"):
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users_db:
+            flash('Username already exists')
+            return redirect(url_for('register'))
+
+        users_db["username"] = username
+        users_db["password"] = password
+        flash('Registration successful! Please log in.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return f"Welcome, {session['username']}! <a href='/logout'>Logout</a>"
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ mongo_client.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import os
+
+import pymongo
+load_dotenv()
+
+database_url = os.getenv("BD_URL")
+
+client = pymongo.MongoClient(database_url)
+
+db = client["banking"]
+col = db["customer"]
+
+query = {"name": "AKvarsha"}
+
+data = col.find_one(query)
+print(data)
+
+
+
+# Replace with your MongoDB URI
+# client = pymongo.MongoClient(database_url)
+
+# Access the admin database
+# admin_db = client.admin
+
+# Run the serverStatus command
+# server_status = admin_db.command("serverStatus")
+
+# Extract connection details
+# connections = server_status.get("connections")
+#
+# print("Current connections:", connections.get("current"))
+# print("Available connections:", connections.get("available"))
+# print("Total created connections:", connections.get("totalCreated"))
