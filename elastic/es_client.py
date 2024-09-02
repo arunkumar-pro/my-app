2763d86f6e1aed8d1641744af72187ddaeb0105c
arunkumar-pro <arunkumarrdckap@gmail.com>
2024-09-02 16:24:02 +0530

    Update es_client.py

--- elastic/es_client.py
+++ elastic/es_client.py
@@ -42,7 +42,7 @@ def create_es_client():
 
         # Ping to check if the connection is successful
         if es.ping():
-            create_index(es, mapping, index=ES_INDEX)
+            # create_index(es, mapping, index=ES_INDEX)
             print("Connected successfully")
             print("Connected to Elasticsearch with Basic Authentication")
             res["message"] = "Connected to Elasticsearch and Mapping  with Basic Authentication"
